5fcf8e8a848634cb83a6aef72c1e7707d5164375
anveetpal01 <166736761+anveetpal01@users.noreply.github.com>
2025-08-28 20:51:46 +0530

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,12 +2,12 @@
 {
     public class Product
     {
-        public int Id { get; set; }   // Primary Key
+        public int Id { get; set; } 
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public string Category { get; set; }
-        public bool IsActive { get; set; } = true;  // Soft delete flag
+        public bool IsActive { get; set; } = true; 
     }
 }
